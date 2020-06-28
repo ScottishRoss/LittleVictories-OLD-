05190284eaf8e3e78de03a6bff34aafa5c39c433
Ross Henderson <ross.scottish@gmail.com>
2020-06-28 21:33:43 +0100

    Commit

--- LittleVictories/LittleVictories/LittleVictories/obj/Debug/netstandard2.0/AddVictory.xaml.g.cs
+++ LittleVictories/LittleVictories/LittleVictories/obj/Debug/netstandard2.0/AddVictory.xaml.g.cs
@@ -17,12 +17,8 @@ namespace LittleVictories {
     public partial class AddVictory : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
-        private global::Xamarin.Forms.Entry Editor;
-        
-        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(AddVictory));
-            Editor = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Entry>(this, "Editor");
         }
     }
 }
