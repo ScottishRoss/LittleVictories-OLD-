8d3167184aba84bebc697742fc45f4a9741a03bf
Ross Henderson <ross.scottish@gmail.com>
2020-06-28 21:25:06 +0100

    First Draft

---
+++ LittleVictories/LittleVictories/LittleVictories/YourVictories.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xamarin.Forms;
+using LittleVictories.Models;
+
+namespace LittleVictories
+{
+    public partial class YourVictories : ContentPage
+    {
+        public YourVictories()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            victoryView.ItemsSource = await App.Database.GetVictoriesAsync();
+        }
+
+        async void OnVictoryAddedClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AddVictory
+            {
+                BindingContext = new Victory()
+            });
+        }
+
+        async void OnVictoryViewItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem != null)
+            {
+                await Navigation.PushAsync(new ViewVictory
+                {
+                    BindingContext = e.SelectedItem as Victory
+                });
+            }
+        }
+    }
+}
---
+++ LittleVictories/Victory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace LittleVictories.Models
+{
+    public class Victory
+    {
+        public string Filename { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
