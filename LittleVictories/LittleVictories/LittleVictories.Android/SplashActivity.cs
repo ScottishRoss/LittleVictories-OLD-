abc8f2649e8b5280cf2abcef778d953129770df4
Ross Henderson <ross.scottish@gmail.com>
2020-07-26 18:31:30 +0100

    Changed to Android 10 API

--- LittleVictories/LittleVictories/LittleVictories.Android/SplashActivity.cs
+++ LittleVictories/LittleVictories/LittleVictories.Android/SplashActivity.cs
@@ -1,8 +1,8 @@
 ﻿using Android.App;
 using Android.Content;
 using Android.OS;
-using Android.Support.V7.App;
 using Android.Util;
+using AndroidX.AppCompat.App;
 using System.Threading.Tasks;
 
 namespace LittleVictories.Droid
