6db5c452e9a7b394f23a8a15660aab171649eeb0
Ross Henderson <ross.scottish@gmail.com>
2020-10-13 12:08:35 +0100

    - Committing work

---
+++ LittleVictories/LittleVictories/LittleVictories/Services/INotificationRegistrationService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace LittleVictories.Services
+{
+    public interface INotificationRegistrationService
+    {
+        Task DeregisterDeviceAsync();
+        Task RegisterDeviceAsync(params string[] tags);
+        Task RefreshRegistrationAsync();
+    }
+
+}
